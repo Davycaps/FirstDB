58c7190046e0166adcb1ecfe20fcd5e8e536264d
Davide Capicchioni <116790994+Davycaps@users.noreply.github.com>
2023-04-28 08:11:59 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+//connessione al database
+
+SQLiteConnection cn1 = new SQLiteConnection("chinook.db");
+
+var tblArtists = cn1.Query<Artist>("select * from artists");
+
+Console.WriteLine( $"in questa tabella cisono {tblArtists.Count} record");
+
+//fine main... inizio dichiarazioni classi-----------------------
+public class Artist
+{
+    public int ArtistId{get;set;}
+    public string Name{get;set;}
+}
